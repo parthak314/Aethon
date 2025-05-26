54c36aee166a44319bbe5552275d6d235a43859b
parthak314 <parthakfree@gmail.com>
2025-05-26 18:01:53 +0100

    Boilerplate project setup done

---
+++ py/backend.py
@@ -0,0 +1,6 @@
+"""
+
+"""
+
+from conn-sonar import SonarConnection
+
---
+++ py/conn-sonar.py
@@ -0,0 +1,9 @@
+class SonarConnection:
+    def __init__(self, host, token):
+        self.host = host
+        self.token = token
+
+    def get_projects(self):
+        pass
+
+    
